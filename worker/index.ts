741d85055ef3854ef06017b05d9b5c77a1da9614
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-08-11 01:56:36 +0530

    not able to revice ips of ec2 instacte

--- worker/index.ts
+++ worker/index.ts
@@ -3,9 +3,13 @@ import {
   AutoScalingClient,
   SetDesiredCapacityCommand,
   DescribeAutoScalingInstancesCommand,
+  
 } from "@aws-sdk/client-auto-scaling";
 import { Request, Response } from "express";
 import express from "express";
+import { EC2Client, DescribeInstancesCommand
+ 
+} from "@aws-sdk/client-ec2";
 
 const app = express();
 
@@ -31,14 +35,27 @@ const client = new AutoScalingClient({
     accessKeyId: process.env.AWS_ACCESS_KEY!,
     secretAccessKey: process.env.AWS_ACCESS_SECRET!,
   },
+  
+
 });
 
 
-async function refreshMachineList() {
+async function  refreshMachineList() {
   const command = new DescribeAutoScalingInstancesCommand({});
   try {
     const data = await client.send(command);
     console.log(data);
+
+    //do this for all instance it check weather it exist in array and if its doesnt it will push otherwise it wont push in the array
+    // ALL_MACHINE.push(data.AutoScalingInstances);
+
+
+    // const ec2InsatcnceCOmmandnew= new DescribeInstancesCommand({
+    //   InstanceIds:data.AutoScalingInstances?.map(x => x.InstanceId)
+    // })
+
+    // const ec2Response = awiat EC2Client.send(ec2InsatcnceCOmmandnew)   //idk why this is not working will figure out later
+    // console.timeLog();
   
   } catch (error) {
     console.error("Error fetching auto-scaling instances:", error);
