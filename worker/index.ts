2f624710077b1a7aace6da59e2ce4f4f00b1bb0e
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-08-11 01:23:32 +0530

    idk what i am doing honestly

--- worker/index.ts
+++ worker/index.ts
@@ -2,27 +2,57 @@ import "dotenv/config";
 import {
   AutoScalingClient,
   SetDesiredCapacityCommand,
+  DescribeAutoScalingInstancesCommand,
 } from "@aws-sdk/client-auto-scaling";
+import { Request, Response } from "express";
+import express from "express";
+
+const app = express();
+
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Welcome to the server");
+  await refreshMachineList(); 
+});
+
+
+type Machine = {
+  ip: string;
+  isUsed: boolean;
+  assignedProject?: string;
+};
+
+const ALL_MACHINE: Machine[] = [];
+
+
+const client = new AutoScalingClient({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_ACCESS_SECRET!,
+  },
+});
+
+
+async function refreshMachineList() {
+  const command = new DescribeAutoScalingInstancesCommand({});
+  try {
+    const data = await client.send(command);
+    console.log(data);
+  
+  } catch (error) {
+    console.error("Error fetching auto-scaling instances:", error);
+  }
+}
+
+
+refreshMachineList();
+setInterval(refreshMachineList, 1000 * 10); 
+
 
 if (!process.env.AWS_ACCESS_KEY || !process.env.AWS_ACCESS_SECRET) {
   throw new Error("AWS credentials are missing from .env");
 }
 
-const client = new AutoScalingClient({
-  region: "ap-south-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_ACCESS_SECRET,
-  },
-});
 
-(async () => {
-  const command = new SetDesiredCapacityCommand({
-    AutoScalingGroupName: "vscode-asg",
-    DesiredCapacity: 2,
-    HonorCooldown: true,
-  });
-
-  const data = await client.send(command);
-  console.log(data);
-})();
+app.listen(4000)
